3c75df0dfa9ad7d12f4bf21196f311aadfbe272d
Zayn <firerageofficial@gmail.com>
2025-08-08 18:43:28 +0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,20 @@
+# simple operators assignment
+
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+x = 3
+x = x + 1
+print(x)
+
+print(a > b)
+print(a == b)
+
+c = True
+d = False
+print(c and d)
---
+++ assignment 2.py
@@ -0,0 +1,18 @@
+# control flow try
+
+num = 5
+
+if num == 5:
+    print("num is 5")
+elif num > 5:
+    print("num is big")
+else:
+    print("num is small")
+
+for i in range(3):
+    print("hello", i)
+
+x = 0
+while x < 2:
+    print("x is", x)
+    x = x + 1
